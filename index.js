5e8eaa9a44a557b189ec493d90ae0b47d83b999b
Miriam Anyango <minaoanya@gmail.com>
2022-06-11 20:26:26 +0300

    commit tree 09b7d7b7841405fdb0076bcdf400eb93628a468a

---
+++ index.js
@@ -0,0 +1,12 @@
+let divide = function(){
+    return 2000/100
+}
+
+
+/*const add = (parameter1, parameter2) => parameter1 + parameter2;
+add(2,3); //=> 5*/
+
+const square = x => x*x;
+ 
+const add = (parameter1, parameter2) => parameter1 + parameter2;
+
